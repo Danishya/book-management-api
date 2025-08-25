92cc0c347b3503fefa43105f60a6e8ca92fc13b6
Danishya-Shanmuganathan <sdanishya28@gmail.com>
2025-08-25 14:01:31 +0530

    structuring the db

--- app.py
+++ app.py
@@ -4,3 +4,9 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books.db'
 db = SQLAlchemy(app)
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    author = db.Column(db.String(100), nullable=False)
+    year = db.Column(db.Integer, nullable=False)
