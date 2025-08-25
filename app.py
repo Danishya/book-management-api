41bbca54a2c5c04fd3f958c9efbac52df8fc7aa6
Danishya-Shanmuganathan <sdanishya28@gmail.com>
2025-08-25 14:15:06 +0530

    DELETE operation-remove

--- app.py
+++ app.py
@@ -70,3 +70,14 @@ def update_book(book_id):
         "author": book.author,
         "year": book.year
     }})
+
+#Delete book by id
+@app.route('/books/<int:book_id>', methods=['DELETE'])
+def delete_book(book_id):
+    book = Book.query.get(book_id)
+    if not book:
+        return jsonify({"error": "Book not found"}), 404
+    db.session.delete(book)
+    db.session.commit()
+    return jsonify({"message": "Book deleted!"})
+
