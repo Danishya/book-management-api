dc8b46545c066d90b56064a9b4641042fef72eb1
Danishya-Shanmuganathan <sdanishya28@gmail.com>
2025-08-25 14:05:40 +0530

    create db tables

--- app.py
+++ app.py
@@ -5,8 +5,14 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books.db'
 db = SQLAlchemy(app)
 
+#database model
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     author = db.Column(db.String(100), nullable=False)
     year = db.Column(db.Integer, nullable=False)
+
+#create db table
+with app.app_context():
+    db.create_all()
+
